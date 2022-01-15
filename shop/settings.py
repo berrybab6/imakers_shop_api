2ca3fd4faeb9450dde61342aacde617c0899ddf7
betty arega <bettyarega3@gmail.com>
2022-01-15 10:31:39 +0300

    vendor init

--- shop/settings.py
+++ shop/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'shop',
-        'USER': 'myprojectuser',
-        'PASSWORD': 'password',
+        'USER': 'postgres',
+        'PASSWORD': 'beth',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
